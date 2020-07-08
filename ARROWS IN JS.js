57094335d5841532dbd9ce972aebca61b5a42f29
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2020-07-08 17:36:49 +0530

    Add files via upload
    UPDATED

---
+++ ARROWS IN JS.js
@@ -0,0 +1,111 @@
+//                                     USING ARROWS IN JAVASCRIPT
+
+
+
+// // 1
+
+
+
+// let guest = () => {
+//     console.log("good morning")
+// }
+// guest();
+
+
+
+// // 2
+
+
+
+// let add = (a, b, c) => {
+//     console.log(a + b + c);
+// }
+// add(1, 2, 3);
+
+
+
+
+// // 3
+
+
+
+// let half = (num) =>{
+//     console.log(num/2);
+// }
+// half(23);
+
+
+
+
+
+// // 4
+
+
+// let square = (num) =>{
+//     console.log(num**2);
+// }
+// square(3);
+
+
+
+
+// // 5
+
+
+
+// setTimeout( ()=>{
+//     console.log("Good Boy");
+// },3000);
+
+
+
+
+
+
+
+// // 6                WEBSITE VIRUS MADE BY ME
+
+
+
+// setInterval( () =>{
+//     alert("TIME IS running out")
+//     a = new Date();
+//     console.log(a);
+//     inner.innerHTML = a;
+// },5000 );
+
+
+
+
+
+// // 7
+
+// let Average_finder = (a,b,c) =>{
+//     console.log((a+b+c)/Average_finder.length);
+// }
+// Average_finder(12,2,2);
+
+
+
+
+
+
+// // 8
+
+
+
+// let object = {
+//     name:"Abhishek Nayak",
+//     subject:"Python",
+//     friends:["adarsh","little","kenchuri"],
+//     speak(){
+//         this.friends.forEach((a) => {
+//             console.log(this.name);
+//             // console.log(this.subject + " is a fav subject of mine" )
+//             console.log("KUCKDOO KOO " + a);
+//             console.log("\n");
+//         });
+//     }
+// }
+
+// object.speak();

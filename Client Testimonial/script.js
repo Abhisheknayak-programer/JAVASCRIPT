6e56daa7f704d61b5ca67ad76a78960ebd0e40b2
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-07-19 16:40:33 +0530

    (Swiper.js) Client Testimonial Design

---
+++ Client Testimonial/script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    loop:true,
+});
